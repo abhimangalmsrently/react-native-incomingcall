e00b2882eed21b3608d5d0169ca4509de2d38bac
HarshaR1642 <sriharsha@rently.com>
2023-03-16 20:12:02 +0530

    2nd commit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ const RNVoipCall = require('react-native-voip-call');
 
 const isIOS = Platform.OS === 'ios';
 
-const IncomingCall = isIOS ? RNVoipCall : NativeModules.IncomingCall;
+const IncomingCall = isIOS
+  ? { ...RNVoipCall, ...RNVoipCall.RNVoipPushKit }
+  : NativeModules.IncomingCall;
 
 export default IncomingCall;
